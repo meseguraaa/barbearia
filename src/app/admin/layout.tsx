8d43ce916eee16d90535f876b439fb887a0b6950
meseguraaa <brunoleal2005@yahoo.com.br>
2025-12-08 08:02:37 -0300

    Menu do admin virou side bar e na lateral

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -3,18 +3,18 @@ import { AdminNav } from "@/components/admin-nav";
 
 export default function AdminLayout({ children }: { children: ReactNode }) {
   return (
-    <div className="min-h-screen bg-background-primary text-content-primary flex flex-col">
-      <header className="">
-        <div className="max-w-7xl mx-auto flex h-16 items-center justify-between gap-4 px-4">
-          <AdminNav />
-        </div>
-      </header>
+    <div className="min-h-screen bg-background-primary text-content-primary">
+      {/* Sidebar fixa à esquerda */}
+      <AdminNav />
 
-      <main className="flex-1 py-6">
-        <div className="max-w-7xl mx-auto">
-          <section className="px-6">{children}</section>
-        </div>
-      </main>
+      {/* Conteúdo deslocado pra direita pra não ficar embaixo da sidebar */}
+      <div className="pl-56">
+        <main className="min-h-screen py-6">
+          <div className="max-w-7xl mx-auto">
+            <section className="px-6">{children}</section>
+          </div>
+        </main>
+      </div>
     </div>
   );
 }
